3f8238b1c4151ba04121b2072ce2179e34be798c
xiaoshengweixiang <84439883+xiaoshengweixiang@users.noreply.github.com>
2023-03-14 16:13:10 +0800

    Update example1.m

--- example1.m
+++ example1.m
@@ -1,13 +1,13 @@
 N=10;
 state=4;
 w=1;v=4;
-T0=[0,w,w,0;w,0,0,w;w,0,0,w;0,w,w,0];
-Tx=zeros(state);
-Tx(1,3)=v;
-Tx(2,4)=v;
-Ty=zeros(state);
-Ty(1,2)=v;
-Ty(3,4)=v;
-bands(T0,Tx,Ty,N);
-transmission(T0,Tx,Ty,N);
+epi0=[0,w,w,0;w,0,0,w;w,0,0,w;0,w,w,0];
+tx=zeros(state);
+tx(1,3)=v;
+tx(2,4)=v;
+ty=zeros(state);
+ty(1,2)=v;
+ty(3,4)=v;
+bands(epi0,tx,ty,N);
+transmission(epi0,tx,ty,N);
 
