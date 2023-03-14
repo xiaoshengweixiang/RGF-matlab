1fccd9f5dc52c536632c554a1347d0cb30bfbd18
xiaoshengweixiang <84439883+xiaoshengweixiang@users.noreply.github.com>
2023-03-14 18:36:15 +0800

    Add files via upload

--- example1.m
+++ example1.m
@@ -1,21 +1,11 @@
-%This is a 2D SSH model
-
-N=10;% width of the y direction
-
-%Input corresponding epi,tx,ty
-state=4;
-w=1;v=4;
-epi0=[0,w,w,0;w,0,0,w;w,0,0,w;0,w,w,0];
-tx=zeros(state);
-tx(1,3)=v;
-tx(2,4)=v;
-ty=zeros(state);
-ty(1,2)=v;
-ty(3,4)=v;
-
+%This is a basic lead model without magnetic
+e0=0.1;t=1.2;
+N=10;
+epi0=e0;
+tx=t;
+ty=t;
 %Use bands function to obtain band structure
 bands(epi0,tx,ty,N);
 
 %Use transmission function to obtain Transmittance
 transmission(epi0,tx,ty,N);
-
