4d013cdb9b58052b1bbcc42f595963d9bc9fe7f4
xiaoshengweixiang <84439883+xiaoshengweixiang@users.noreply.github.com>
2023-03-14 18:28:42 +0800

    Add files via upload

--- example1.m
+++ example1.m
@@ -1,4 +1,8 @@
-N=10;
+%This is a 2D SSH model
+
+N=10;% width of the y direction
+
+%Input corresponding epi,tx,ty
 state=4;
 w=1;v=4;
 epi0=[0,w,w,0;w,0,0,w;w,0,0,w;0,w,w,0];
@@ -8,6 +12,10 @@ tx(2,4)=v;
 ty=zeros(state);
 ty(1,2)=v;
 ty(3,4)=v;
+
+%Use bands function to obtain band structure
 bands(epi0,tx,ty,N);
+
+%Use transmission function to obtain Transmittance
 transmission(epi0,tx,ty,N);
 
